e7fd9a5900014129fef2cd7ff9607f75050fc040
2022tgoel <tishi.tng@gmail.com>
2022-02-25 14:48:37 -0500

    add bit files

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Projects based off of the labs at: https://www.seas.upenn.edu/~ese171/
 
 I used the DIGILENT Nexys 4 DDR FPGA Board: https://digilent.com/reference/programmable-logic/nexys-4-ddr/reference-manual
+
+Out-of-the-Box Demo: https://github.com/Digilent/Nexys-4-DDR-OOB 
--- vga/vga_top_level.vhd
+++ vga/vga_top_level.vhd
@@ -14,13 +14,18 @@ end vga_top_level;
 architecture Structural of vga_top_level is
 	signal S : STD_LOGIC_VECTOR (1 downto 0) := "00";
 	signal hcount, vcount : STD_LOGIC_VECTOR (9 downto 0) := "0000000000";
+	signal hc, vc : integer range 0 to 1000 := 0;
 	signal VGA_CLK : STD_LOGIC; -- the 25 Mhz clock
 begin 
 	CLOCK_DIVIDER : entity work.clock_divider port map (BOARD_CLK => CLK, CLK => VGA_CLK);
 	STATE : entity work.state_generator port map (X => X, CLK => VGA_CLK, RESET => RESET, S => S, LED => DISPLAY_CATHODE);
 	CONTROLLER : entity work.vga_controller port map (CLK => VGA_CLK, RESET => RESET, hcount => hcount, vcount => vcount, 
 										  HSYNC => VGA_HSYNC, VSYNC => VGA_VSYNC);
-	RGBGEN : entity work.RGBGenerator port map (S => S, hcount => hcount, vcount => vcount, vga_r => VGA_R, vga_b => VGA_B, 
-									vga_g => VGA_G);
+	hc <= conv_integer(hcount);
+	vc <= conv_integer(vcount);
+	VGA_R <= "1111" when (S = "00" or (S = "11" and (vc < 120 or vc >= 360))) else "0000";
+	VGA_G <= "1111" when (S = "01" or (S = "11" and ((vc >= 120 and vc < 240) or vc >= 360))) else "0000";
+	VGA_B <= "1111" when (S = "10" or (S = "11" and (vc >= 240))) else "0000";
+
 	DISPLAY_ANODE <= '0';
 end Structural;	
